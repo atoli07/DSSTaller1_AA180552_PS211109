72fd0aed9735bfef27e8e675769d144dc7a0bd78
Samuel-Pana <89950478+Samuel-Pana@users.noreply.github.com>
2022-03-04 23:33:25 -0600

    Inicio de sesion

---
+++ InicioSesion.php
@@ -0,0 +1,40 @@
+<?php
+
+          //Matriz de usuarios y contraseñas
+          $nombres_usuarios = ['admin', 'usuario1', 'usuario2'];
+          $contrasenas      = ['12345', 'abcde', '123abc'];
+
+          //Recontruimos la matriz para comparar 
+          $usuarios = array_combine( $nombres_usuarios, $contrasenas );
+
+            // Obtenemos el usuario y la contraseña del objeto POST.
+              $usuario    = $_POST['user'];
+              $contrasena = $_POST['password'];
+
+
+            
+            // Comprobamos que el usuario existe en la matriz.
+            if( ! empty( $usuarios[$usuario] ) ) {
+
+                //Comprobamos que el usuario coincide con la contraseña.
+                if( $usuarios[$usuario] === $contrasena ) {
+                
+                    // Comprobar el tipo de usuario.
+                    if( $usuario === 'admin') {
+                        print ('hola admin');
+                        //linea de codigo de enlace para enviar al usuario 1 a su pagina de eventos 
+                    } elseif ( $usuario === 'usuario1') {
+                        print ('hola  usuario 2');
+                        //linea de codigo de enlace para enviar al usuario 2 a su pagina de eventos 
+                    } else {
+                        print ('hola  usuario 3');
+                        //linea de codigo de enlace para enviar al usuario 3 a su pagina de eventos 
+                    }
+                   
+                } else {
+                    print ('Contraseña inválida');
+                }
+                } else {
+                    print ('Usuario inválido');
+              }      
+?>
