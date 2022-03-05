c6b4169f435f9b9f88b1a9fda44fef9cb53e8582
Samuel-Pana <89950478+Samuel-Pana@users.noreply.github.com>
2022-03-05 00:41:40 -0600

    Corrección en InicioSesion

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <h1>Inicio de Sesión</h1>
         </div>
         <div class="contenedor">
-            <form action="eventos.php" method="post">
+            <form action="InicioSesion.php" method="post">
                 <div class="inputIcon">
                     <input type="text" name="user" id="user"
                      placeholder="Ingrese su nombre de usuario" required>
